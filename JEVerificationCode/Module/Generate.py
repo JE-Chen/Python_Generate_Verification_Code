66bc4779b43b77fc5dbdc593d743a663acef78d4
JE-Chen <zenmailman@gmail.com>
2020-12-25 20:54:19 +0800

    test

--- JEVerificationCode/Module/Generate.py
+++ JEVerificationCode/Module/Generate.py
@@ -91,7 +91,7 @@ class GenerateVerificationCode:
     def generate_base64_image(self, code_count, font_size, save=False):
 
         code_image = self.generate_picture()
-        valid, code_image = self.generate_code(code_count, code_image, font_size)
+        valid, code_image = self.generate_code(5, code_image, 40)
         code_image = self.generate_noise(code_image)
 
         if save:
